b0e2e9f9c2d735456cd80a2d315761e59e3ba04e
Biju Kattiyil Murali <biju.murali@metro-markets.de>
2021-11-25 07:07:30 +0100

    code cleanup

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = props => {
   return (
     <nav className={classes.nav}>
       <ul>
         {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
+          <React.Fragment>
+            <li>
+              <a href="/">Users</a>
+            </li>
+            <li>
+              <a href="/">Admin</a>
+            </li>
+            <li>
+              <button onClick={props.onLogout}>Logout</button>
+            </li>
+          </React.Fragment>
         )}
       </ul>
     </nav>
